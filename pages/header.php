80424888c7cb598d13835a20b4b751c6df82afbc
Hadjirul22 <149591464+Hadjirul22@users.noreply.github.com>
2024-11-26 00:44:32 +0800

    loading

--- pages/header.php
+++ pages/header.php
@@ -44,7 +44,20 @@ if (isset($_SESSION['first_name']) && !isset($_SESSION['welcome_shown'])) {
 </head>
 
 <body>  
+	<!-- Preloader -->
+    <div class="preloader">
+            <div class="loader">
+                <div class="loader-outter"></div>
+                <div class="loader-inner"></div>
 
+                <div class="indicator"> 
+                    <svg width="16px" height="12px">
+                        <polyline id="back" points="1 6 4 6 6 11 10 1 12 6 15 6"></polyline>
+                        <polyline id="front" points="1 6 4 6 6 11 10 1 12 6 15 6"></polyline>
+                    </svg>
+                </div>
+            </div>
+        </div>
 
 		<header class="header">
         <div class="header-inner">
